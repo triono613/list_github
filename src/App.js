4d864a0e3da5d765e3b622738c2af86070070be1
triono613 <triono.triono1@gmail.com>
2021-12-04 10:40:11 +0700

    add semantic ui css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> new
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react'
 
-export default App;
+export default function App() {
+
+  const [name,setName] = useState('');
+  const [userName,setUsername] = useState('');
+  const [following,setFollowing] = useState('');
+  const [repos,setRepos] = useState('');
+  const [avatar,setAvatar] = useState('');
+  const [userInput,setUserInput] = useState('');
+  const [error,setError] = useState(null);
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
